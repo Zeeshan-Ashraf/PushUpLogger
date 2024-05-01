9ab325bad3b036dfc03a1b05705e32f69b0a59ce
Md Zeeshan Ashraf <idzeeid@gmail.com>
2024-05-02 05:08:31 +0530

    sqlalchemy orm crud filter like order_by desc

---
+++ service/userService.py
@@ -0,0 +1,48 @@
+import logging
+from dataclasses import dataclass
+
+from database.userRepository import UserRepository
+from model.user import User
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+@dataclass
+class UserService:
+    userRepository: UserRepository = UserRepository()
+
+    def getUser(self, id: int) -> User:
+        try:
+            user: User = self.userRepository.findUserById(id)
+            return user
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
+    def save(self, user: User) -> None:
+        try:
+            self.userRepository.save(user)
+        except Exception as e:
+            logging.exception(e)
+            raise e
+
+    def getAllUsers(self):
+        try:
+            users: list[User] = self.userRepository.findAllUser()
+            return users
+        except Exception as e:
+            logging.exception(e)
+            raise e
+
+    def deleteUser(self, id: int):
+        try:
+            self.userRepository.deleteUser(id)
+        except Exception as e:
+            raise e
+
+    def searchUser(self, minId: int, like: str):
+        try:
+            return self.userRepository.findAllUserByGtIdDesc(minId, like)
+        except Exception as e:
+            raise e
